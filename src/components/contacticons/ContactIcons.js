8e98e1c7ced7ae346f784d4f465bfb206ebdba98
Ayouub <ayoubyysaidyy@gmail.com>
2023-05-30 20:01:41 +0100

    Debugging gh pages

--- src/components/contacticons/ContactIcons.js
+++ src/components/contacticons/ContactIcons.js
@@ -6,7 +6,7 @@ import linkedin from './icons/icons8-linkedin-40.png'
 
 function ContactIcons() {
   return (
-    <div>
+    <div style={{position :"fixed"}}>
         <img className="icon" id="discord-click" style={{marginTop: "100px"}} title="Click to copy!" src={discord} />
         <a href="https://github.com/Ayouubyy" target="_blank"><img className="icon" src={github} /></a>
         <a href="https://www.linkedin.com/in/ayoub-said-32ba56254/" target="_blank"><img className="icon" src={linkedin} /></a>
