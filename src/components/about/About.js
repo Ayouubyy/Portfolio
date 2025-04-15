2716e974221f3014a09f893f8bd3434d4aabf975
Ayouub <ayoubyysaidyy@gmail.com>
2025-04-15 20:28:22 +0100

    Updated information

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,7 @@ function About() {
     <div className="about-div">
          <p id="about" className="about-header">About me:</p>
           <p className="about-text">
-            My name is Ayoub Said and I'm a 17-year-old software developer from Tunisia. I discovered my passion for programming while studying Mathematics, and I've been building my skills ever since. I'm a driven and creative problem solver who is always looking for new challenges and ways to improve. In my free time, I enjoy exploring new technologies and working on personal projects. I'm excited to see where my career in software development will take me and I'm always eager to collaborate with others on innovative projects.
+          Hi, I'm Ayoub Said — a 19-year-old software developer from Tunisia and a pre-engineering student specializing in Maths and Physics. My journey into programming began through my love for mathematics, and since then, I’ve been continuously sharpening my skills through personal projects and exploration of new technologies.
           </p>
     </div>
   )
