49b82adba2b3b87e689582bbb8447a6cecdcfba3
Said Ayoub <73258396+Ayouubyy@users.noreply.github.com>
2025-06-18 05:28:33 +0100

    Update Linkedin

--- src/components/contacticons/ContactIcons.js
+++ src/components/contacticons/ContactIcons.js
@@ -9,7 +9,7 @@ function ContactIcons() {
     <div style={{position :"fixed"}}>
         <img className="icon" style={{marginTop: "100px"}} onClick={() => navigator.clipboard.writeText('ayouubyy')} title="Click to copy!" src={discord} />
         <a href="https://github.com/Ayouubyy"  target="_blank"><img style={{marginTop: "200px"}} className="icon" src={github} /></a>
-        <a href="https://www.linkedin.com/in/ayoub-said-32ba56254/" target="_blank"><img style={{marginTop: "300px"}} className="icon" src={linkedin} /></a>
+        <a href="https://www.linkedin.com/in/ayoub-said-300031316/" target="_blank"><img style={{marginTop: "300px"}} className="icon" src={linkedin} /></a>
     </div>
   )
 }
